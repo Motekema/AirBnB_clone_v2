031a5b204e6b3a12520b784f213c135d3c3ed46f
Motekema <motekema@gmail.com>
2023-12-19 03:43:06 -0800

    My commit

--- models/review.py
+++ models/review.py
@@ -1,10 +1,39 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
-from models.base_model import BaseModel
+"""
+Review Module for the HBNB project.
 
+Defines the Review class to store review information. In a database-driven
+environment, it uses SQLAlchemy to map the Review class to the 'reviews' table,
+including columns for text, place_id, and user_id. In a non-database
+environment, it defines attributes but leaves them empty.
 
-class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+Attributes:
+    - text (str): The text content of the review.
+    - place_id (str): The ID of the place associated with the review.
+    - user_id (str): The ID of the user who wrote the review.
+
+In a database-driven environment (storage_type == 'db'):
+    - It uses SQLAlchemy to map the Review class to the 'reviews' table.
+    - It includes columns for text, place_id, and user_id.
+    - It establishes foreign key relationships with the Place and User classes.
+
+In a non-database environment (storage_type != 'db'):
+    - It defines attributes but leaves them empty.
+"""
+
+from sqlalchemy.sql.schema import ForeignKey
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+
+class Review(BaseModel, Base):
+    """Review class to store review information."""
+    __tablename__ = 'reviews'
+    
+    if storage_type == 'db':
+        text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
