c338b65835b913383fe7f33d4427bf4b87dc090d
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2023-12-20 16:05:57 +0200

    My commit

--- models/amenity.py
+++ models/amenity.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This are the amenity class"""
+"""This is the amenity class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
@@ -14,3 +14,4 @@ class Amenity(BaseModel, Base):
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
     place_amenities = relationship("Place", secondary=place_amenity)
+
