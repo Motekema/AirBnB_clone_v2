bf2d5e9baa3903dfbf91ccd6862db455bb273340
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2023-12-20 15:34:27 +0200

    My commit

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This is the file storage class for AirBnB"""
+"""This are the file storage class for AirBnB"""
 import json
 from models.base_model import BaseModel
 from models.user import User
@@ -12,10 +12,10 @@ import shlex
 
 
 class FileStorage:
-    """This class serializes instances to a JSON file and
-    deserializes JSON file to instances
+    """This is class serializes instances to a JSON file and
+    deserializes JSON file instances
     Attributes:
-        __file_path: path to the JSON file
+        __file_path: path to ta JSON file
         __objects: objects will be stored
     """
     __file_path = "file.json"
@@ -24,7 +24,7 @@ class FileStorage:
     def all(self, cls=None):
         """returns a dictionary
         Return:
-            returns a dictionary of __object
+            returns the dictionary of __object
         """
         dic = {}
         if cls:
@@ -57,7 +57,7 @@ class FileStorage:
             json.dump(my_dict, f)
 
     def reload(self):
-        """serialize the file path to JSON file path
+        """serialize the file path the JSON file path
         """
         try:
             with open(self.__file_path, 'r', encoding="UTF-8") as f:
@@ -68,13 +68,13 @@ class FileStorage:
             pass
 
     def delete(self, obj=None):
-        """ delete an existing element
+        """ delete the existing element
         """
         if obj:
             key = "{}.{}".format(type(obj).__name__, obj.id)
             del self.__objects[key]
 
     def close(self):
-        """ calls reload()
+        """ calls to reload()
         """
         self.reload()
