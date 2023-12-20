3f5b3c7479b4b9368e1c934ee87a1fcc248b98af
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2023-12-20 15:38:36 +0200

    My commit

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" """
+"""Testing save method and if it update """
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
 from models.base_model import BaseModel
@@ -22,7 +22,7 @@ class test_Amenity(test_basemodel):
         self.value = Amenity
 
     def test_name2(self):
-        """ """
+        """Testing save method and if it update """
         new = self.value()
         self.assertEqual(type(new.name), str)
 
