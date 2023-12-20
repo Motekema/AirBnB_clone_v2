5145aae025dedef23135444b6072ac5c4639dcfd
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2023-12-20 14:37:34 +0200

    My commit

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" """
+""" test """
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
 
 
 class test_Amenity(test_basemodel):
-    """ """
+    """test console """
 
     def __init__(self, *args, **kwargs):
         """ """
