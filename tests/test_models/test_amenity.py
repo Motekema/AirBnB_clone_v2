85a80e6beaf05c199a468cfccfe722fc2a3244e3
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2023-12-20 14:17:06 +0200

    My commit

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" """
+""" test amenity"""
 import os
 from models.amenity import Amenity
 from tests.test_models.test_base_model import test_basemodel
 
 
-class test_Amenity(test_basemodel):
+class TestAmenity(test_basemodel):
     """ It tests class"""
 
     def __init__(self, *args, **kwargs):
@@ -14,9 +14,10 @@ class test_Amenity(test_basemodel):
         self.name = "Amenity"
         self.value = Amenity
 
-    def test_name2(self):
+    def test_name(self):
         """It is testing name type """
         new = self.value()
         self.assertEqual(type(new.name), str if
                          os.getenv('HBNB_TYPE_STORAGE') != 'db' else
                          type(None))
+
