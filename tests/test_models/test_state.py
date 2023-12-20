27b2d924a641f365e9d686948a6948eea0695154
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2023-12-20 14:41:56 +0200

    My commit

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" """
+"""Modal """
 from tests.test_models.test_base_model import test_basemodel
 from models.state import State
 
 
 class test_state(test_basemodel):
-    """ """
+    """Test """
 
     def __init__(self, *args, **kwargs):
         """ """
@@ -14,6 +14,6 @@ class test_state(test_basemodel):
         self.value = State
 
     def test_name3(self):
-        """ """
+        """Return """
         new = self.value()
         self.assertEqual(type(new.name), str)
