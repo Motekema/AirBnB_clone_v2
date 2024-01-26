93e4b2ad3daa3923383eadf2b405714c2e9955c4
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-01-26 10:10:39 +0200

    Update 1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -4,18 +4,19 @@ This script starts a Flask web application with two routes.
 """
 
 from flask import Flask
-
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
+def index():
+    """ return Hello HBNB!"""
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Displays 'HBNB'"""
+    """ return HBNB"""
     return 'HBNB'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
